6b7169ed41ad4a8b455d735a1ae0e2f7755acfaa
NILESH KUMAR PATRALEKH <199477385+nileshpatralekh@users.noreply.github.com>
2025-12-26 00:41:16 +0530

    Update SW to Network-First for HTML and force immediate updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'typinghub-v2';
+const CACHE_NAME = 'typinghub-v3'; // Increment version
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -8,6 +8,8 @@ const ASSETS_TO_CACHE = [
 ];
 
 self.addEventListener('install', (event) => {
+    // Force immediate activation
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             console.log('Opened cache');
@@ -16,29 +18,47 @@ self.addEventListener('install', (event) => {
     );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            // Cache hit - return response
-            if (response) {
-                return response;
-            }
-            return fetch(event.request);
-        })
+self.addEventListener('activate', (event) => {
+    // Take control of all clients immediately
+    event.waitUntil(
+        Promise.all([
+            self.clients.claim(),
+            caches.keys().then((cacheNames) => {
+                return Promise.all(
+                    cacheNames.map((cacheName) => {
+                        if (cacheName !== CACHE_NAME) {
+                            return caches.delete(cacheName);
+                        }
+                    })
+                );
+            })
+        ])
     );
 });
 
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
+self.addEventListener('fetch', (event) => {
+    // Network First strategy for HTML (Navigation) requests
+    if (event.request.mode === 'navigate') {
+        event.respondWith(
+            fetch(event.request)
+                .then((response) => {
+                    // Update cache with new version
+                    const responseClone = response.clone();
+                    caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request, responseClone);
+                    });
+                    return response;
                 })
-            );
-        })
-    );
+                .catch(() => {
+                    return caches.match(event.request);
+                })
+        );
+    } else {
+        // Cache First for other assets
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                return response || fetch(event.request);
+            })
+        );
+    }
 });
